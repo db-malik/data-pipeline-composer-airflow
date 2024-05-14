653cf0400fa119f3d00fe074c603273bc32be3a9
db-malik <dbmalek92@gmail.com>
2024-05-14 13:53:03 +0200

    test

--- tests/test_dags.py
+++ tests/test_dags.py
@@ -1,48 +1,60 @@
 import unittest
+from unittest.mock import patch, MagicMock
 from airflow.models import DagBag
 
 
-class TestDAG(unittest.TestCase):
+class TestDBoubaDataExtractionAndLoadingDAG(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.dagbag = DagBag(dag_folder="../dags/", include_examples=False)
-        print(
-            "Import errors:", cls.dagbag.import_errors
-        )  # Add this line to see import errors
+        cls.dagbag = DagBag(dag_folder="dags/", include_examples=False)
+        cls.dag = cls.dagbag.get_dag(dag_id="dbouba_data_extraction_and_loading_dag")
 
     def test_dag_loaded(self):
-        """Check that the DAG file is syntactically correct and can be successfully imported."""
-        dags = self.dagbag.dags
-        self.assertIn(
-            "dbouba_data_transformation_and_loading",
-            dags,
-            "DAG 'dbouba_data_transformation_and_loading' is not present in the DAG bag",
-        )
+        """Ensure that the DAG is in the DagBag."""
+        self.assertIn("dbouba_data_extraction_and_loading_dag", self.dagbag.dags)
         self.assertEqual(len(self.dagbag.import_errors), 0, "DAGs failed to import")
 
-    def test_task_count(self):
-        """Check if the correct number of tasks have been added to the DAG."""
-        dag_id = "dbouba_data_transformation_and_loading"
-        dag = self.dagbag.get_dag(dag_id)
-        self.assertEqual(len(dag.tasks), 6, "Number of tasks is not as expected")
+    @patch(
+        "airflow.providers.google.cloud.transfers.gcs_to_bigquery.GCSToBigQueryOperator.execute"
+    )
+    @patch(
+        "airflow.providers.google.cloud.transfers.gcs_to_gcs.GCSToGCSOperator.execute"
+    )
+    def test_task_dependencies(self, mock_gcs_to_bq, mock_gcs_to_gcs):
+        """Check the task dependencies within the dag."""
+        task_ids = self.dag.task_dict.keys()
+        self.assertSetEqual(
+            set(task_ids),
+            {
+                "start",
+                "load_to_bigquery",
+                "move_file_to_archive",
+                "move_file_to_error",
+                "trigger_second_dag",
+                "end",
+            },
+        )
 
-    def test_dependencies_of_tasks(self):
-        """Check the dependencies of tasks in the DAG."""
-        dag_id = "dbouba_data_transformation_and_loading"
-        dag = self.dagbag.get_dag(dag_id)
-        dependencies = {
-            "start_task": {"second_task"},
-            "second_task": {"end_task"},
-            "end_task": set(),
-        }
+        load_to_bq_task = self.dag.task_dict["load_to_bigquery"]
+        archive_task = self.dag.task_dict["move_file_to_archive"]
+        error_task = self.dag.task_dict["move_file_to_error"]
+        trigger_task = self.dag.task_dict["trigger_second_dag"]
+        end_task = self.dag.task_dict["end"]
 
-        for task_id, downstream_list in dependencies.items():
-            task = dag.get_task(task_id)
-            self.assertEqual(
-                set(task.downstream_task_ids),
-                downstream_list,
-                f"Dependencies for {task_id} are incorrect",
-            )
+        # Check downstream tasks for load_to_bq_task
+        self.assertTrue(
+            list(load_to_bq_task.downstream_task_ids)
+            == ["move_file_to_archive", "move_file_to_error"]
+        )
+
+        # Ensure archive and error tasks lead to trigger task
+        self.assertTrue(
+            list(archive_task.downstream_task_ids) == ["trigger_second_dag"]
+        )
+        self.assertTrue(list(error_task.downstream_task_ids) == ["trigger_second_dag"])
+
+        # Ensure trigger task leads to end task
+        self.assertTrue(list(trigger_task.downstream_task_ids) == ["end"])
 
 
 if __name__ == "__main__":
